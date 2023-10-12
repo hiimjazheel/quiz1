2f7514f14e65f6d79a314fb7cb75b2a8eaf2a198
Jazheel Jia <jazheel.jiasiit01@gmail.com>
2023-10-12 20:25:23 +0800

    First commit

---
+++ act1.py
@@ -0,0 +1,14 @@
+#Display the total amount of diesel to pay per liter
+#Value of price and liter
+price = 70
+liter = 5
+#Formula for the total amount to pay
+total = price * liter
+#Display
+print ("price per liter: ", price)
+print ("price per diesel: ",liter)
+print("the total amount to pay is: ",total)
+
+
+  
+  
